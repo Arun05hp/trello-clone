8007c9b15e9e290caeee51a8d68d96294844a6bb
Arun Kumar <kumar05.962013@gmail.com>
2022-12-18 20:06:04 +0530

    dashboard navbar

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardNavbar from "../../components/navbar/DashboardNavbar";
+
+const index = () => {
+  return (
+    <div>
+      <DashboardNavbar />
+    </div>
+  );
+};
+
+export default index;
